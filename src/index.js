1ba2488b61101f79f0fa374db766f354aa440b25
AhmedRamadan009 <105159451+AhmedRamadan009@users.noreply.github.com>
2022-08-07 15:26:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HasRoute } from 'react-router-dom';
 
 
 
